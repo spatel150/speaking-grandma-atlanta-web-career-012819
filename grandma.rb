462d9756187b61c72df4022fa3a0ee6fa930f750
Samidh Patel <spatel13456@gmail.com>
2019-01-05 04:16:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,6 +20,6 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   else 
-    return "NO, NOT SINCE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end 
