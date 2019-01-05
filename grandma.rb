0f9911a99e078f7797c0a847f6e56fe4e4a9efd7
Samidh Patel <spatel13456@gmail.com>
2019-01-05 04:13:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    return "I"
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else 
+    return "NO "
 end 
